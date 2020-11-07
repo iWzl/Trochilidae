478428fb5503a1bd87d1f50da8554f32cfaf365d
Leo <LeoWang@Inslabs.org>
2020-11-07 22:48:39 +0800

    测试相关调整

--- example/src/main/java/com/upuphub/trochilidae/example/controller/TrochilidaeTestController.java
+++ example/src/main/java/com/upuphub/trochilidae/example/controller/TrochilidaeTestController.java
@@ -9,6 +9,8 @@ import com.upuphub.trochilidae.web.common.http.DefaultHttpRsp;
 import com.upuphub.trochilidae.web.common.http.ServiceResponseMessage;
 import com.upuphub.trochilidae.web.common.lang.HttpMediaType;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -35,6 +37,32 @@ public class TrochilidaeTestController {
 
     @PostMapping(path = "/user",consumes = HttpMediaType.APPLICATION_JSON_UTF8_VALUE,produces = HttpMediaType.APPLICATION_JSON_UTF8_VALUE)
     public ServiceResponseMessage<UserRsp> printRequest(@RequestBody UserReq userReq){
+        UserRsp userRsp = new UserRsp();
+        userRsp.setAge(userReq.getAge() + 1);
+        userRsp.setName(userReq.getName() + "-Name");
+        userRsp.setDescription(userReq.getDescription() + UUID.randomUUID().toString());
+        return ServiceResponseMessage.createBySuccessCodeMessage(userRsp);
+    }
+
+    @GetMapping(path = "/test",produces = HttpMediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Map<String,String> printRequest1(){
+        Map<String, String> map = new HashMap<String, String>(4);
+        map.put("hello", "World");
+        return map;
+    }
+
+
+    @PutMapping(path = "/user",consumes = HttpMediaType.APPLICATION_JSON_UTF8_VALUE,produces = HttpMediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ServiceResponseMessage<UserRsp> printRequesto(@RequestBody UserReq userReq){
+        UserRsp userRsp = new UserRsp();
+        userRsp.setAge(userReq.getAge() + 1);
+        userRsp.setName(userReq.getName() + "-Name");
+        userRsp.setDescription(userReq.getDescription() + UUID.randomUUID().toString());
+        return ServiceResponseMessage.createBySuccessCodeMessage(userRsp);
+    }
+
+    @DeleteMapping(path = "/user",consumes = HttpMediaType.APPLICATION_JSON_UTF8_VALUE,produces = HttpMediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ServiceResponseMessage<UserRsp> printRequestoo(@RequestBody UserReq userReq){
         UserRsp userRsp = new UserRsp();
         userRsp.setAge(userReq.getAge() + 1);
         userRsp.setName(userReq.getName() + "-Name");
