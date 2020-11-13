df22ebb22c82f16fbe0162c6788b43e714be55a6
Leo <LeoWang@Inslabs.org>
2020-11-13 15:47:20 +0800

    POM管理的调整和相关说明

---
+++ core/src/main/java/com/upuphub/trochilidae/core/ioc/AutowiredBeanInitialization.java
@@ -0,0 +1,10 @@
+package com.upuphub.trochilidae.core.ioc;
+
+/**
+ * 自动注入的Bean初始化管理器
+ *
+ * @author Inspiration S.P.A Leo
+ * @date create time 2020-11-13 15:29
+ **/
+public class AutowiredBeanInitialization {
+}
